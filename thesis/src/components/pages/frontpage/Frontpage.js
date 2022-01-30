c9167688a9a183491d65adc4fd47143146786229
Jennina Färm <jennina.farm@exove.fi>
2022-01-30 20:08:00 +0200

    Frontpafe_skeleton: Move input and label component to their own repository

--- thesis/src/components/pages/frontpage/Frontpage.js
+++ thesis/src/components/pages/frontpage/Frontpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-import Input from '../../atoms/Input'
+import Input from '../../atoms/input/Input'
 
 const Frontpage = () => {
   return (
