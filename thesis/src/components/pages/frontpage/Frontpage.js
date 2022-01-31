6928cbc876ac5d2906d6663b89453c2453b39387
Jennina Färm <jennina.farm@exove.fi>
2022-01-31 08:47:58 +0200

    Frontpage_navigation: Finetune Frontpage.js and basic-elements.scss

--- thesis/src/components/pages/frontpage/Frontpage.js
+++ thesis/src/components/pages/frontpage/Frontpage.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Button from '../../atoms/button/Button';
 // import PropTypes from 'prop-types';
-
 
 import FieldsetOneInput from '../../molecules/fieldsets/FieldsetOneInput';
 
