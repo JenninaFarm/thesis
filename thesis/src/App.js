973e9bc6a597f849565fd94d97af4cb050e1905a
Jennina Färm <jennina.farm@exove.fi>
2022-01-31 09:19:09 +0200

    Navigation_frontpage: Add routes and route for the frontpage

--- thesis/src/App.js
+++ thesis/src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from "react-router-dom";
+
 import './Styles.scss';
 
 import Frontpage from './components/pages/frontpage/Frontpage';
@@ -5,7 +7,9 @@ import Frontpage from './components/pages/frontpage/Frontpage';
 const App = () => {
   return (
     <div className="dance-vote">
-      <Frontpage />
+      <Routes>
+        <Route path="/" element={<Frontpage />} />
+      </Routes>
     </div>
   );
 }
